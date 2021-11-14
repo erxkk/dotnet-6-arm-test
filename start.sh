18619bfeb37a13ab72fd54464a68d11a6e0a3eac
erxkk <ErikBuennig@gmail.com>
2021-11-14 16:36:50 +0100

    haha pretty loops :)

--- start.sh
+++ start.sh
@@ -4,24 +4,24 @@
 
 for i in {5,6}; do
     for t in {build,prebuilt}; do
-        echo "=> building $i-$t"
+        echo "==> building $i-$t"
         docker build --tag dotnet-$i-$t ./dotnet-$i-$t &>> out/dotnet-$i-$t.build.log
 
-        echo "=> inspecting $i-$t image"
+        echo "==> inspecting $i-$t image"
         docker image inspect dotnet-$i-$t &>> out/dotnet-$i-$t.image.json
 
-        echo "=> running $i-$t"
+        echo "==> running $i-$t"
         docker run --name dotnet-$i-$t dotnet-$i-$t &>> out/dotnet-$i-$t.run.log
 
-        echo "=> inspecting $i-$t container"
+        echo "==> inspecting $i-$t container"
         docker inspect dotnet-$i-$t &>> out/dotnet-$i-$t.image.json
 
-        echo "=> cleaning $i-$t"
+        echo "==> cleaning $i-$t"
         docker rm dotnet-$i-$t &>> /dev/null
         docker image rm dotnet-$i-$t &>> /dev/null
     done
 
-    echo "inspecting mcr $i images"
+    echo "=> inspecting mcr $i images"
     docker inspect mcr.microsoft.com/dotnet/sdk:$i.0 &>> out/mcr-$i-runtime.image.json
     docker inspect mcr.microsoft.com/dotnet/runtime:$i.0 &>> out/mcr-$i-runtime.image.json
 done
