b0198a20d97a7304f94b1cb7788926b8b87cbf22
erxkk <ErikBuennig@gmail.com>
2021-11-14 16:34:21 +0100

    Fixed loops

--- start.sh
+++ start.sh
@@ -2,19 +2,19 @@
 
 [[ ! -d 'out' ]] && mkdir out
 
-for i in {5, 6}; do
-    for t in {'build', 'prebuilt'}; do
+for i in {5,6}; do
+    for t in {build,prebuilt}; do
         echo "=> building $i-$t"
         docker build --tag dotnet-$i-$t ./dotnet-$i-$t &>> out/dotnet-$i-$t.build.log
 
         echo "=> inspecting $i-$t image"
-        docker image inspect dotnet-$i-$t &>> out/dotnet-$i-$t.image.log
+        docker image inspect dotnet-$i-$t &>> out/dotnet-$i-$t.image.json
 
         echo "=> running $i-$t"
         docker run --name dotnet-$i-$t dotnet-$i-$t &>> out/dotnet-$i-$t.run.log
 
         echo "=> inspecting $i-$t container"
-        docker inspect dotnet-$i-$t &>> out/dotnet-$i-$t.image.log
+        docker inspect dotnet-$i-$t &>> out/dotnet-$i-$t.image.json
 
         echo "=> cleaning $i-$t"
         docker rm dotnet-$i-$t &>> /dev/null
@@ -22,12 +22,11 @@ for i in {5, 6}; do
     done
 
     echo "inspecting mcr $i images"
-    docker inspect mcr.microsoft.com/dotnet/sdk:$i.0 &>> out/mcr-$i-runtime.image.log
-    docker inspect mcr.microsoft.com/dotnet/runtime:$i.0 &>> out/mcr-$i-runtime.image.log
-
-    echo "remvoing residual build container"
+    docker inspect mcr.microsoft.com/dotnet/sdk:$i.0 &>> out/mcr-$i-runtime.image.json
+    docker inspect mcr.microsoft.com/dotnet/runtime:$i.0 &>> out/mcr-$i-runtime.image.json
 done
 
-$_DOCKER_RES=$(docker image ls | awk '/^<none>/{ print $3 }')
-[[ -n $DOCKER_RES ]] && docker image rm $_DOCKER_RES
+echo "remvoing residual build container"
+_DOCKER_RES=$(docker image ls | awk '/^<none>/{ print $3 }')
+[[ -n $DOCKER_RES ]] && echo $_DOCKER_RES | xargs docker image rm
 
